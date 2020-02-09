7e49a3507181448016d725b4aa954cc8e7a9454a
SiddharthJ1 <59508895+SiddharthJ1@users.noreply.github.com>
2020-02-09 21:01:53 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,15 @@
+class Form{
+constructor(){
+
+}
+display(){
+var title= createElement('h2')
+title.html("MultiCar")
+title.position(130,0)
+var input= createInput("Name")
+input.position (130,160)
+var button= createButton("Play")
+button.position(250,200)
+
+}
+}
